b8184dbca38c35acfbdedf44e33aadbc5ffd1d24
sahinealam <msahine961@gmail.com>
2026-05-07 19:24:07 +0600

    banner section

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -5,6 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 
 const Banner = () => {
+    // banner section
     return (
         <div>
             <div className="carousel w-full rounded-xl overflow-hidden relative">
